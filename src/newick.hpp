142dfd82d3281170ec3fbfe3cfd0109dcd0acd8a
cpockrandt <noreply-github@cpockrandt.de>
2020-11-10 02:23:06 +0100

    code cleanup

--- src/newick.hpp
+++ src/newick.hpp
@@ -7,16 +7,13 @@ struct newick_node
 {
     std::string label = "";
     float branch_length = .0f;
-    newick_node* left_child = NULL;
-    newick_node* right_child = NULL;
-    newick_node* parent = NULL;
+    newick_node* left = NULL;
+    newick_node* right = NULL;
+    newick_node* parent = NULL; // TODO: I think we can easily eliminate this field
 };
 
-void newick_parse(std::string& str, newick_node* node, int i, const bool debug)
+void newick_parse(std::string& str, newick_node* node)
 {
-    if (debug)
-        std::cout << std::string(2*i, '-') << "Current string: " << str << '\n';
-
     if (str[0] != '(' && str[0] != ',' && str[0] != ')' && str[0] != ':')
     {
         while (str[0] != '(' && str[0] != ',' && str[0] != ')' && str[0] != ':')
@@ -24,8 +21,6 @@ void newick_parse(std::string& str, newick_node* node, int i, const bool debug)
             node->label += str[0];
             str = str.substr(1);
         }
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Set label " << node->label << '\n';
 
         str = str.substr(1); // remove ':'
 
@@ -36,34 +31,26 @@ void newick_parse(std::string& str, newick_node* node, int i, const bool debug)
             str = str.substr(1);
         }
         node->branch_length = std::stof(len);
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Set branch length " << std::to_string(node->branch_length) << '\n';
     }
 
     if (str[0] == '(')
     {
         newick_node* left = new newick_node;
         left->parent = node;
-        node->left_child = left;
+        node->left = left;
         //
         str = str.substr(1); // remove '('
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Created left child\n";
-        newick_parse(str, left, i + 1, debug);
+        newick_parse(str, left);
 
         newick_node* right = new newick_node;
         right->parent = node;
-        node->right_child = right;
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Created right child\n";
-        newick_parse(str, right, i + 1, debug);
+        node->right = right;
+        newick_parse(str, right);
     }
 
     if (str[0] == ',')
     {
         str = str.substr(1); // remove ','
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Go up one parent to go to the right next!\n";
         return; // go up one
     }
 
@@ -84,18 +71,16 @@ void newick_parse(std::string& str, newick_node* node, int i, const bool debug)
             str = str.substr(1);
         }
         node->parent->branch_length = std::stof(len);
-        if (debug)
-            std::cout << std::string(2*i, '-') << "Set branch length " << std::to_string(node->parent->branch_length) << '\n';
     }
 }
 
-inline newick_node* newick_parse(std::string& str, const bool debug)
+inline newick_node* newick_parse(std::string& str)
 {
     // TODO: remove trailing semicolon
     newick_node* root = new newick_node;
     root->parent = NULL;
 
-    newick_parse(str, root, 0, debug);
+    newick_parse(str, root);
 
     return root;
 }
@@ -104,7 +89,7 @@ inline std::string newick_print(newick_node* n)
 {
     assert(n != NULL);
 
-    if (n->left_child == NULL && n->right_child == NULL)
+    if (n->left == NULL && n->right == NULL)
     {
         return n->label + ":" + std::to_string(n->branch_length);
     }
@@ -113,15 +98,15 @@ inline std::string newick_print(newick_node* n)
         std::string bl = "";
         if (n->parent != NULL)
             bl = ":" + std::to_string(n->branch_length);
-        return "(" + newick_print(n->left_child) + "," + newick_print(n->right_child) + ")" + bl;
+        return "(" + newick_print(n->left) + "," + newick_print(n->right) + ")" + bl;
     }
 }
 
 inline void newick_free(newick_node* n)
 {
-    if (n->left_child != NULL)
-        newick_free(n->left_child);
-    if (n->right_child != NULL)
-        newick_free(n->right_child);
+    if (n->left != NULL)
+        newick_free(n->left);
+    if (n->right != NULL)
+        newick_free(n->right);
     free(n);
 }
