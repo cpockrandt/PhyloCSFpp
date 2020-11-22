5b73f3c1123f912b175eb96faf67c00e6dd7c733
cpockrandt <noreply-github@cpockrandt.de>
2020-11-22 22:30:40 +0100

    ECM float -> double, also buffer was too small by a few bytes

--- src/ecm.hpp
+++ src/ecm.hpp
@@ -4,21 +4,21 @@
 
 struct empirical_codon_model
 {
-    float matrix[64][64]; // TODO: only store triangle and flatten array for cache locality, also don't store diag since it's 0
-    float codon_freq[64];
+    double matrix[64][64]; // TODO: only store triangle and flatten array for cache locality, also don't store diag since it's 0
+    double codon_freq[64];
 
     // makes matrix symmetric and set diagonal to 0
     bool open(const char* path)
     {
         FILE* stream = fopen(path, "r");
 
-        char line[1024];
+        char line[4096];
 
         // read file
         uint16_t line_id = 1; // the first line would be a zeros, that's why it is skipped
         matrix[0][0] = .0f;
 
-        while (fgets(line, 1024, stream))
+        while (fgets(line, 4096, stream))
         {
             uint16_t field_id = 0;
 
@@ -27,7 +27,7 @@ struct empirical_codon_model
             if (line_id <= 63) // codon matrix
             {
                 matrix[line_id][line_id] = .0f; // diagonal is 0
-                for (float val = strtof(line2, &end); line2 != end; val = strtof(line2, &end))
+                for (double val = strtod(line2, &end); line2 != end; val = strtod(line2, &end))
                 {
                     line2 = end;
                     matrix[field_id][line_id] = val; // symmetric matrix
@@ -38,7 +38,7 @@ struct empirical_codon_model
             }
             else if (line_id == 65) // codon frequencies
             {
-                for (float val = strtof(line2, &end); line2 != end; val = strtof(line2, &end))
+                for (double val = strtod(line2, &end); line2 != end; val = strtod(line2, &end))
                 {
                     line2 = end;
                     codon_freq[field_id] = val;
@@ -59,7 +59,7 @@ struct empirical_codon_model
         {
             for (uint16_t j = 0; j < 64; ++j)
             {
-                printf("%.3f\t", matrix[i][j]);
+                printf("%f\t", matrix[i][j]);
             }
             printf("\n");
         }
@@ -68,7 +68,7 @@ struct empirical_codon_model
 
         for (uint16_t i = 0; i < 64; ++i)
         {
-            printf("%.3f\t", codon_freq[i]);
+            printf("%f\t", codon_freq[i]);
         }
         printf("\n");
     }
