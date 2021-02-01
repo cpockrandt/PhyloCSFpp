cb91b1e32570d43811aa4416add201903d75cdcf
cpockrandt <noreply-github@cpockrandt.de>
2021-02-02 00:31:22 +0100

    added wrapper for annotation

--- src/phylocsf++.cpp
+++ src/phylocsf++.cpp
@@ -1,8 +1,8 @@
-#include "common.hpp"
 #include "arg_parse.hpp"
 
 #include "phylocsf++tracks.hpp"
 #include "phylocsf++region.hpp"
+#include "phylocsf++annotate.hpp"
 
 int main(int argc, char **argv)
 {
@@ -12,8 +12,9 @@ int main(int argc, char **argv)
                   "Please consider citing: Harry Frankfurt: On Bullshit. Princeton University Press,\n"
                   "                        Princeton, New Jersey 2005");
 
-    args.add_subprogram("region", "Computes PhyloCSF scores, ancestral sequence composition sores and branch length scores for entire alignments from MAF and FASTA files. Outputs them in BED format.");
     args.add_subprogram("tracks", "Computes PhyloCSF and Power tracks for each codon and all 6 frames from alignments from MAF and FASTA files. Outputs them in wig files.");
+    args.add_subprogram("scores", "Computes PhyloCSF scores, ancestral sequence composition sores and branch length scores for entire alignments from MAF and FASTA files. Outputs them in BED format.");
+    args.add_subprogram("annotate", "Scores the CDS features in a GFF/GTF file using precomputed tracks or by computing a multiple sequence alignment from scratch (requires MMseqs2 to be installed).");
 
     args.add_option("help" , /*'e',*/ ArgParse::Type::FLAG, "Prints this help message. Run `phylocsf++ tracks --help` for help messages on the tools", ArgParse::Level::GENERAL, false);
 
@@ -23,9 +24,13 @@ int main(int argc, char **argv)
     {
         return main_tracks(argc - 1, argv + 1);
     }
-    else if (args.get_selected_subprogram() == "region")
+    else if (args.get_selected_subprogram() == "scores")
     {
         return main_region(argc - 1, argv + 1);
+    }
+    else if (args.get_selected_subprogram() == "annotate")
+    {
+        return main_annotate(argc - 1, argv + 1);
     }
 
     return 0;
--- src/phylocsf++annotate.hpp
+++ src/phylocsf++annotate.hpp
@@ -1,3 +1,5 @@
+#include "common.hpp"
+
 #include "arg_parse.hpp"
 #include "models.hpp"
 
@@ -5,138 +7,155 @@
 
 #include <omp.h>
 
-void run_regions(const std::string & alignment_path, const Model & model, const RegionCLIParams & params)
-{
 
-}
+#include <stdio.h>
+#include <sys/wait.h>
 
-int main_region(int argc, char **argv)
+//void run_annotate(const std::string & alignment_path, const Model & model, const AnnotateCLIParams & params)
+//{
+//
+//}
+
+int main_annotate(int /*argc*/, char** /*argv*/)
 {
     ArgParse args("phylocsf++ annotate",
                   "Computes PhyloCSF scores for CDS features in GFF/GTF files and outputs them in \n"
-                  " \n"
-                  "sores and branch length scores can be computed as well. Only one forward frame \n"
-                  "is computed, i.e., for other frames reverse the alignments and/or remove the \n"
-                  "first one or two bases."); // For ORF finding and scoring of transcripts in GTF/GFF "
-                  // "files, check out PhyloCSF++Anno: https://github.com/cpockrandt/phylocsfppanno");
+                  "the same format. You can either pass track files (in wig format) or have the \n"
+                  "multiple sequence alignment computed for the CDS (this requires mmseqs2 to be \n"
+                  "installed).");
 
-    RegionCLIParams params;
-
-    std::string model_list = get_list_of_models();
-
-    std::string default_strategy;
-    switch (params.strategy)
+    // check whether mmseqs is installed
+    int ret = system("mmseqs --help > /dev/null 2>&1");
+    if (WEXITSTATUS(ret))
     {
-        case MLE:
-            default_strategy = "MLE";
-            break;
-        case FIXED:
-            default_strategy = "FIXED";
-            break;
-        case OMEGA:
-            default_strategy = "OMEGA";
-            break;
+        print_error_msg("ERROR: MMseqs2 seems not to be installed.\n");
+        exit(-1);
     }
 
-    args.add_option("strategy", ArgParse::Type::STRING, "PhyloCSF scoring algorithm: MLE, FIXED or OMEGA. Default: " + default_strategy, ArgParse::Level::GENERAL, false);
-    args.add_option("comp-phylo", ArgParse::Type::BOOL, "Compute the PhyloCSF score for each alignment. Default: " + std::to_string(params.comp_phylo), ArgParse::Level::GENERAL, false);
-    args.add_option("comp-anc", ArgParse::Type::BOOL, "Compute the ancestral sequence composition score (only in MLE and FIXED mode). Default: " + std::to_string(params.comp_anc), ArgParse::Level::GENERAL, false);
-    args.add_option("comp-bls", ArgParse::Type::BOOL, "Compute the branch length score (confidence). Default: " + std::to_string(params.comp_bls), ArgParse::Level::GENERAL, false);
-
-    args.add_option("threads", ArgParse::Type::INT, "Parallelize scoring of multiple MSAs in a file using multiple threads. Default: " + std::to_string(params.threads), ArgParse::Level::GENERAL, false);
-    args.add_option("output", ArgParse::Type::STRING, "Path where tracks in wig format will be written. If it does not exist, it will be created. Default: output files are stored next to the input files.", ArgParse::Level::GENERAL, false);
-
-    args.add_option("mapping", ArgParse::Type::STRING, "If the MSAs don't use common species names (like Human, Chimp, etc.) you can pass a two-column tsv file with a name mapping.", ArgParse::Level::GENERAL, false);
-    args.add_option("species", ArgParse::Type::STRING, "Comma separated list of species to reduce <model> to a subset of species to improve running time, e.g., \"Human,Chimp,Seaturtle\"", ArgParse::Level::GENERAL, false);
-    args.add_option("model-info", ArgParse::Type::STRING, "Output the organisms included in a specific model. Included models are: " + model_list + ".", ArgParse::Level::GENERAL, false);
-    // TODO: ignore sequences not occuring in model (instead of failing)
-
-    args.add_positional_argument("model", ArgParse::Type::STRING, "Path to parameter files, or one of the following predefined models: " + model_list + ".", true);
-    args.add_positional_argument("alignments", ArgParse::Type::STRING, "One or more files containing multiple sequence alignments. Formats: MAF and multi FASTA. Multiple MSAs can be stored in a single file separated by empty lines.", true, true);
-    args.parse_args(argc, argv);
-
-    // additional help strings
-    if (args.is_set("model-info"))
+    ret = system("mmseqs result2dnamsa --help > /dev/null 2>&1");
+    if (WEXITSTATUS(ret))
     {
-        const std::string & model_name = args.get_string("model-info");
-        return print_model_info(model_name);
+        print_error_msg("ERROR: A version of MMseqs2 seems to be installed that is too old. `mmseqs result2dnamsa --help` does not seem to work.\n");
+        exit(-1);
     }
 
-    args.check_args(); // check here because if "--model-info" is set, we don't want to require mandatory arguments
-
-    // retrieve flags/options that were set
-    if (args.is_set("threads"))
-        params.threads = args.get_int("threads");
-
-    if (args.is_set("strategy"))
-    {
-        std::string strategy = args.get_string("strategy");
-        std::transform(strategy.begin(), strategy.end(), strategy.begin(), toupper);
-
-        if (strategy == "MLE")
-            params.strategy = MLE;
-        else if (strategy == "FIXED")
-            params.strategy = FIXED;
-        else if (strategy == "OMEGA")
-            params.strategy = OMEGA;
-        else
-        {
-            print_error_msg("Please choose a valid strategy (MLE, FIXED or OMEGA)");
-            return -1;
-        }
-    }
-
-    if (args.is_set("comp-phylo"))
-        params.comp_phylo = args.get_bool("comp-phylo");
-    if (args.is_set("comp-anc"))
-        params.comp_anc = args.get_bool("comp-anc");
-    if (args.is_set("comp-bls"))
-        params.comp_bls = args.get_bool("comp-bls");
-
-    if (params.strategy == OMEGA && params.comp_anc)
-    {
-        print_error_msg("The ancestral sequence composition cannot be computed in the Omega mode!");
-        return -1;
-    }
-
-    if (!params.comp_phylo && !params.comp_anc && !params.comp_bls)
-    {
-        print_error_msg("At least one score needs to be computed (phylo, anc or bls)!");
-        return -1;
-    }
-
-    if (args.is_set("output"))
-    {
-        params.output_path = args.get_string("output");
-        // create a directory if it doesn't exist yet
-        if (create_directory(params.output_path))
-            print_info_msg("Created the output directory.");
-    }
-
-    if (args.is_set("mapping"))
-    {
-        std::string mapping_file = args.get_string("mapping");
-        update_sequence_name_mapping(mapping_file);
-    }
-
-    if (!args.is_set_positional("model") || !args.is_set_positional("alignments"))
-    {
-        print_error_msg("No model or alignments provided.");
-        return -1;
-    }
-
-    const std::string alignment_path = args.get_positional_argument("alignments");
-
-    // load and prepare model
-    Model model;
-    load_model(model, args.get_positional_argument("model"), args.get_string("species"), false, 0, "");
-
-    // run for every alignment file
-    for (uint16_t i = 1; i < args.positional_argument_size(); ++i)
-    {
-        // printf("%d, %s\n", i, args.get_positional_argument(i).c_str());
-         run_regions(args.get_positional_argument(i), model, params);
-    }
+//    AnnotateCLIParams params;
+//
+//    std::string model_list = get_list_of_models();
+//
+//    std::string default_strategy;
+//    switch (params.strategy)
+//    {
+//        case MLE:
+//            default_strategy = "MLE";
+//            break;
+//        case FIXED:
+//            default_strategy = "FIXED";
+//            break;
+//        case OMEGA:
+//            default_strategy = "OMEGA";
+//            break;
+//    }
+//
+//    args.add_option("strategy", ArgParse::Type::STRING, "PhyloCSF scoring algorithm: MLE, FIXED or OMEGA. Default: " + default_strategy, ArgParse::Level::GENERAL, false);
+//    args.add_option("comp-phylo", ArgParse::Type::BOOL, "Compute the PhyloCSF score for each alignment. Default: " + std::to_string(params.comp_phylo), ArgParse::Level::GENERAL, false);
+//    args.add_option("comp-anc", ArgParse::Type::BOOL, "Compute the ancestral sequence composition score (only in MLE and FIXED mode). Default: " + std::to_string(params.comp_anc), ArgParse::Level::GENERAL, false);
+//    args.add_option("comp-bls", ArgParse::Type::BOOL, "Compute the branch length score (confidence). Default: " + std::to_string(params.comp_bls), ArgParse::Level::GENERAL, false);
+//
+//    args.add_option("threads", ArgParse::Type::INT, "Parallelize scoring of multiple MSAs in a file using multiple threads. Default: " + std::to_string(params.threads), ArgParse::Level::GENERAL, false);
+//    args.add_option("output", ArgParse::Type::STRING, "Path where tracks in wig format will be written. If it does not exist, it will be created. Default: output files are stored next to the input files.", ArgParse::Level::GENERAL, false);
+//
+//    args.add_option("mapping", ArgParse::Type::STRING, "If the MSAs don't use common species names (like Human, Chimp, etc.) you can pass a two-column tsv file with a name mapping.", ArgParse::Level::GENERAL, false);
+//    args.add_option("species", ArgParse::Type::STRING, "Comma separated list of species to reduce <model> to a subset of species to improve running time, e.g., \"Human,Chimp,Seaturtle\"", ArgParse::Level::GENERAL, false);
+//    args.add_option("model-info", ArgParse::Type::STRING, "Output the organisms included in a specific model. Included models are: " + model_list + ".", ArgParse::Level::GENERAL, false);
+//    // TODO: ignore sequences not occuring in model (instead of failing)
+//
+//    args.add_positional_argument("model", ArgParse::Type::STRING, "Path to parameter files, or one of the following predefined models: " + model_list + ".", true);
+//    args.add_positional_argument("alignments", ArgParse::Type::STRING, "One or more files containing multiple sequence alignments. Formats: MAF and multi FASTA. Multiple MSAs can be stored in a single file separated by empty lines.", true, true);
+//    args.parse_args(argc, argv);
+//
+//    // additional help strings
+//    if (args.is_set("model-info"))
+//    {
+//        const std::string & model_name = args.get_string("model-info");
+//        return print_model_info(model_name);
+//    }
+//
+//    args.check_args(); // check here because if "--model-info" is set, we don't want to require mandatory arguments
+//
+//    // retrieve flags/options that were set
+//    if (args.is_set("threads"))
+//        params.threads = args.get_int("threads");
+//
+//    if (args.is_set("strategy"))
+//    {
+//        std::string strategy = args.get_string("strategy");
+//        std::transform(strategy.begin(), strategy.end(), strategy.begin(), toupper);
+//
+//        if (strategy == "MLE")
+//            params.strategy = MLE;
+//        else if (strategy == "FIXED")
+//            params.strategy = FIXED;
+//        else if (strategy == "OMEGA")
+//            params.strategy = OMEGA;
+//        else
+//        {
+//            print_error_msg("Please choose a valid strategy (MLE, FIXED or OMEGA)");
+//            return -1;
+//        }
+//    }
+//
+//    if (args.is_set("comp-phylo"))
+//        params.comp_phylo = args.get_bool("comp-phylo");
+//    if (args.is_set("comp-anc"))
+//        params.comp_anc = args.get_bool("comp-anc");
+//    if (args.is_set("comp-bls"))
+//        params.comp_bls = args.get_bool("comp-bls");
+//
+//    if (params.strategy == OMEGA && params.comp_anc)
+//    {
+//        print_error_msg("The ancestral sequence composition cannot be computed in the Omega mode!");
+//        return -1;
+//    }
+//
+//    if (!params.comp_phylo && !params.comp_anc && !params.comp_bls)
+//    {
+//        print_error_msg("At least one score needs to be computed (phylo, anc or bls)!");
+//        return -1;
+//    }
+//
+//    if (args.is_set("output"))
+//    {
+//        params.output_path = args.get_string("output");
+//        // create a directory if it doesn't exist yet
+//        if (create_directory(params.output_path))
+//            print_info_msg("Created the output directory.");
+//    }
+//
+//    if (args.is_set("mapping"))
+//    {
+//        std::string mapping_file = args.get_string("mapping");
+//        update_sequence_name_mapping(mapping_file);
+//    }
+//
+//    if (!args.is_set_positional("model") || !args.is_set_positional("alignments"))
+//    {
+//        print_error_msg("No model or alignments provided.");
+//        return -1;
+//    }
+//
+//    const std::string alignment_path = args.get_positional_argument("alignments");
+//
+//    // load and prepare model
+//    Model model;
+//    load_model(model, args.get_positional_argument("model"), args.get_string("species"), false, 0, "");
+//
+//    // run for every alignment file
+//    for (uint16_t i = 1; i < args.positional_argument_size(); ++i)
+//    {
+//        // printf("%d, %s\n", i, args.get_positional_argument(i).c_str());
+//         run_regions(args.get_positional_argument(i), model, params);
+//    }
 
     return 0;
 }
