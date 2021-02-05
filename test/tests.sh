aa1741bccbd1d65813c82df2bbf2420193ebd195
cpockrandt <noreply-github@cpockrandt.de>
2021-02-05 03:55:54 +0100

    started adding build-track tests to travis:

--- test/tests.sh
+++ test/tests.sh
@@ -2,22 +2,36 @@
 
 PhyloCSFpp="build/phylocsf++"
 THREADS=$1
+
+### build-tracks
+
+# download data
+wget -m -nd ftp://ftp.ccb.jhu.edu/pub/pocki/phylocsf++/tests/
+
+$PhyloCSFpp build-tracks --genome-length 1065365434 --coding-exons ChickenCodingExons.txt \
+  --output-phylo 1 --output-raw-phylo 1 --output-power 1 --threads $THREADS --output ./results \
+  53birds chr22_25_28_each_30k_bases.maf
+
+diff -qr ./results ./expected_results
+
+### score-msa
+
 species=""
 # --species Lizard,Dolphin,Orangutan,Platypus,Opossum,Weddell_seal,Human,Gibbon,Stickleback,Baboon,Chimp
 # --species anoCar,gasAcu,hg,lepWed,monDom,nomLeu,ornAna,panTro,papHam,ponAbe,turTru
 
 echo "FIXED mode"
-$PhyloCSFpp scores --threads $THREADS --strategy fixed --comp-phylo 1 --comp-anc 1 --comp-bls 1 $species 100vertebrates example/small/chr22.50alignments.maf # no randomization
+$PhyloCSFpp score-msa --threads $THREADS --strategy fixed --comp-phylo 1 --comp-anc 1 --comp-bls 1 $species 100vertebrates example/small/chr22.50alignments.maf # no randomization
 DIFF_FIXED=$(diff example/small/chr22.50alignments.maf.scores example/small/PhyloCSFpp-results/chr22.50alignments.fixed.scores)
 echo -e "${DIFF_FIXED}"
 
 echo "MLE mode"
-$PhyloCSFpp scores --threads $THREADS --strategy mle --comp-phylo 1 --comp-anc 1 --comp-bls 1 100vertebrates example/small/chr22.50alignments.maf
+$PhyloCSFpp score-msa --threads $THREADS --strategy mle --comp-phylo 1 --comp-anc 1 --comp-bls 1 100vertebrates example/small/chr22.50alignments.maf
 DIFF_MLE=$(awk -F$'\t' '($1 != $8 || $2 != $9 || $3 != $10 || $4 != $11 || ($5-$12)^2 > 0.001 || ($6-$13)^2 > 0.001 || $7 != $14)' <(paste example/small/chr22.50alignments.maf.scores example/small/PhyloCSFpp-results/chr22.50alignments.mle.scores))
 echo -e "${DIFF_MLE}"
 
 echo "OMEGA mode"
-$PhyloCSFpp scores --threads $THREADS --strategy omega --comp-phylo 1 --comp-anc 0 --comp-bls 1 100vertebrates example/small/chr22.50alignments.maf
+$PhyloCSFpp score-msa --threads $THREADS --strategy omega --comp-phylo 1 --comp-anc 0 --comp-bls 1 100vertebrates example/small/chr22.50alignments.maf
 DIFF_OMEGA=$(awk -F$'\t' '($1 != $7 || $2 != $8 || $3 != $9 || $4 != $10 || ($5-$11)^2 > 0.1 || $6 != $12)' <(paste example/small/chr22.50alignments.maf.scores example/small/PhyloCSFpp-results/chr22.50alignments.omega.scores))
 #DIFF_OMEGA=$(diff example/small/chr22.50alignments.maf.scores example/small/PhyloCSFpp-results/chr22.50alignments.omega.scores)
 echo -e "${DIFF_OMEGA}"
