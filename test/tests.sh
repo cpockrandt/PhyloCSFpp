169401527a32d4e3f75994efa8d4730d91c48fe2
Christopher Pockrandt <noreply-github@cpockrandt.de>
2021-04-04 01:28:35 +0200

    [TEST] Tests and examples restructured

--- test/tests.sh
+++ test/tests.sh
@@ -6,15 +6,15 @@ THREADS=$1
 ### build-tracks
 
 # download data: wget -m -nd ftp://ftp.ccb.jhu.edu/pub/pocki/phylocsf++/tests/s
-if [ ! -f "test/chr22_25_28_each_30k_bases.maf" ]; then
-  gunzip test/chr22_25_28_each_30k_bases.maf.gz
+if [ ! -f "example/galGal6_chr22_25_28_each_30k_bases.maf" ]; then
+  gunzip example/galGal6_chr22_25_28_each_30k_bases.maf.gz
 fi
 
-$PhyloCSFpp build-tracks --genome-length 1065365434 --coding-exons test/ChickenCodingExons.txt \
+$PhyloCSFpp build-tracks --genome-length 1065365434 --coding-exons example/galGal6_coding_exons.txt \
   --output-phylo 1 --output-raw-phylo 1 --threads $THREADS --output ./build/results \
-  53birds test/chr22_25_28_each_30k_bases.maf
+  53birds example/galGal6_chr22_25_28_each_30k_bases.maf
 
-diff -r ./build/results ./test/expected_results
+diff -r ./build/results ./test/expected_results/build-tracks
 
 ### score-msa
 
@@ -23,19 +23,19 @@ species=""
 # --species anoCar,gasAcu,hg,lepWed,monDom,nomLeu,ornAna,panTro,papHam,ponAbe,turTru
 
 echo "FIXED mode"
-$PhyloCSFpp score-msa --threads $THREADS --strategy fixed --comp-phylo 1 --comp-anc 1 $species 100vertebrates example/small/chr22.50alignments.maf # no randomization
-DIFF_FIXED=$(diff <(awk '!($0 ~ /^#/)' example/small/chr22.50alignments.maf.scores) example/small/PhyloCSFpp-results/chr22.50alignments.fixed.scores) # remove header line
+$PhyloCSFpp score-msa --threads $THREADS --strategy fixed --comp-phylo 1 --comp-anc 1 $species 100vertebrates test/maf-file-small/chr22.50alignments.maf # no randomization
+DIFF_FIXED=$(diff <(awk '!($0 ~ /^#/)' test/maf-file-small/chr22.50alignments.maf.scores) test/maf-file-small/PhyloCSFpp-results/chr22.50alignments.fixed.scores) # remove header line
 echo -e "${DIFF_FIXED}"
 
 echo "MLE mode"
-$PhyloCSFpp score-msa --threads $THREADS --strategy mle --comp-phylo 1 --comp-anc 1 100vertebrates example/small/chr22.50alignments.maf
-DIFF_MLE=$(awk -F$'\t' '($1 != $8 || $2 != $9 || $3 != $10 || $4 != $11 || ($5-$12)^2 > 0.001 || ($6-$13)^2 > 0.001 || $7 != $14)' <(paste <(awk '!($0 ~ /^#/)' example/small/chr22.50alignments.maf.scores) example/small/PhyloCSFpp-results/chr22.50alignments.mle.scores))
+$PhyloCSFpp score-msa --threads $THREADS --strategy mle --comp-phylo 1 --comp-anc 1 100vertebrates test/maf-file-small/chr22.50alignments.maf
+DIFF_MLE=$(awk -F$'\t' '($1 != $8 || $2 != $9 || $3 != $10 || $4 != $11 || ($5-$12)^2 > 0.001 || ($6-$13)^2 > 0.001 || $7 != $14)' <(paste <(awk '!($0 ~ /^#/)' test/maf-file-small/chr22.50alignments.maf.scores) test/maf-file-small/PhyloCSFpp-results/chr22.50alignments.mle.scores))
 echo -e "${DIFF_MLE}"
 
 echo "OMEGA mode"
-$PhyloCSFpp score-msa --threads $THREADS --strategy omega --comp-phylo 1 --comp-anc 0 100vertebrates example/small/chr22.50alignments.maf
-DIFF_OMEGA=$(awk -F$'\t' '($1 != $7 || $2 != $8 || $3 != $9 || $4 != $10 || ($5-$11)^2 > 0.1 || $6 != $12)' <(paste <(awk '!($0 ~ /^#/)' example/small/chr22.50alignments.maf.scores) example/small/PhyloCSFpp-results/chr22.50alignments.omega.scores))
-#DIFF_OMEGA=$(diff example/small/chr22.50alignments.maf.scores example/small/PhyloCSFpp-results/chr22.50alignments.omega.scores)
+$PhyloCSFpp score-msa --threads $THREADS --strategy omega --comp-phylo 1 --comp-anc 0 100vertebrates test/maf-file-small/chr22.50alignments.maf
+DIFF_OMEGA=$(awk -F$'\t' '($1 != $7 || $2 != $8 || $3 != $9 || $4 != $10 || ($5-$11)^2 > 0.1 || $6 != $12)' <(paste <(awk '!($0 ~ /^#/)' test/maf-file-small/chr22.50alignments.maf.scores) test/maf-file-small/PhyloCSFpp-results/chr22.50alignments.omega.scores))
+#DIFF_OMEGA=$(diff test/maf-file-small/chr22.50alignments.maf.scores test/maf-file-small/PhyloCSFpp-results/chr22.50alignments.omega.scores)
 echo -e "${DIFF_OMEGA}"
 
 if [ -n "${DIFF_FIXED}" ] || [ -n "${DIFF_MLE}" ] || [ -n "${DIFF_OMEGA}" ]; then # is any diff non-empty?
